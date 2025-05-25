1ea0bfba4ffc9bee3c0baaa9488afdd78232d2d7
AshtonAnd3 <ashton.anderson2023@outlook.com>
2025-05-25 10:22:33 -0500

    Shortened the csv

--- graph_stock_data.py
+++ graph_stock_data.py
@@ -7,7 +7,7 @@ def plot_gold_prices_from_csv(csv_path):
     data_frame['Date'] = pd.to_datetime(data_frame['Date'])
     data_frame = data_frame.sort_values('Date')
 
-    gold_price_figure = pex.line(data_frame, x='Date', y='Price', title='Gold Price Over 5/24/2024-5/24/2025')
+    gold_price_figure = pex.line(data_frame, x='Date', y='Price', title='Gold Price Over Time')
     gold_price_figure.update_traces(line_color='gold')
     gold_price_figure.update_layout(
         xaxis_title='Time (d)',
